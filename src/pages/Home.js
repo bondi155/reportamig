8fc64cbcda2885d18bab992a308b24008da916c7
bondi155 <78815412+bondi155@users.noreply.github.com>
2024-03-18 18:05:00 -0600

    Add Accesos Directos home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,95 +1,113 @@
 import React, { useState, useEffect } from 'react';
 import '../css/App.css';
-import { Container, Row, Col, Card, Form } from 'react-bootstrap';
+import { Container, Row, Col, Card, Button } from 'react-bootstrap';
 import axios from 'axios';
 import { API_URL } from '../config/config';
-import { FaUserCircle } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+import { FaUserCircle, FaArrowRight } from 'react-icons/fa';
 
 function Home({ form }) {
-  const [totalCalif, setTotalCalif] = useState(0);
   const [error, setError] = useState(null); //pongo el error en un state para mostrarlo en pantalla
   const domainParts = form.username.split('@')[1].split('.');
   const domainName = domainParts[0];
-  const [currentDomain, setCurrentDomain] = useState(domainName);
-  const [dateEval, setDateEval] = useState([]);
+  const [currentDomain] = useState(domainName);
 
- // const labelsNumerics = ['2', '3', '4', '5', '6']; //, '7'
- // const labelsAlphabets = ['A', 'B', 'B+', 'B-', 'C', 'NP'];
- // const labelsAlphabetsTsm = ['A', 'B', 'B+', 'C', 'D'];
-
-//acordarse de setear el error en el catch
+  //acordarse de setear el error en el catch
   return (
     <>
       <Container className='container-custom'>
         {error ? (
           <div>
-            Hubo un problema cargando los graficos..error: Recargue la pagina
-            por favor{error}
+            Hubo un problema cargando el inicio graficos..error: Recargue la
+            pagina por favor{error}
           </div>
-        ) : form.role === 'admin' ? (
-          //pantalla para Administrador
-          <div>
-            {' '}
-            <Card
-              className='mb-3'
-              style={{
-                boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.1)',
-                backgroundColor: '#f8f9fa', // Un gris claro
-              }}
-            >
-              <Card.Header className='d-flex align-items-center'>
-                <Col xs={12} sm={12} md={12} lg={12}>
-                  <h4>
-                    <FaUserCircle className='mb-2' size={35} /> Bienvenido
-                    Administrador <strong>{form.username}</strong>
-                  </h4>
-                </Col>
-              </Card.Header>
-              <Card.Body>
-                {currentDomain === 'admin' ? (
-                  <>
-                    {' '}
-                    <small>
-                      Como administrador tendras accesso a los datos de todas
-                      las empresas :{' '}
-                    </small>{' '}
-                  </>
-                ) : (
-                  //
-                  <Row>
-                    {/* Tarjeta para la Total de datos */}
-                    <Col xs={12} sm={6} lg={6}>
-                      <Card>
-                        <Card.Body>
-                          <Card.Title>blaa</Card.Title>
-                          <div>
-                            <strong>{currentDomain}</strong> desde{' '}
-                            <strong>01/2023</strong>
-                          </div>
-                        </Card.Body>
-                      </Card>
+        ) : form.role === 'admin' && currentDomain === 'admin' ? (
+          // Pantalla para Administrador
+          <Container fluid>
+            <Row className='justify-content-center'>
+              <Col lg={10}>
+                <Card
+                  className='mb-3'
+                  style={{
+                    boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.1)',
+                    backgroundColor: '#f8f9fa',
+                  }}
+                >
+                  <Card.Header className='d-flex align-items-center'>
+                    <Col xs={12} sm={12} md={12} lg={12}>
+                      <h4>
+                        <FaUserCircle className='mb-2' size={35} /> Bienvenido
+                        Administrador <strong>{form.username}</strong>
+                      </h4>
+                      <small>
+                        Como administrador tendrás acceso a todas las
+                        configuraciones.
+                      </small>
                     </Col>
-                    {/* Tarjeta para la Última Evaluación */}
-                    <Col xs={12} sm={6} lg={6}>
-                      <Card>
-                        <Card.Body>
-                          <Card.Title>array de datos mapeado</Card.Title>
-                          {dateEval.map((index, key) => (
-                            <div key={key}>
-                              <strong>{index.nombreColumna}</strong>
-                              <h2>{index.nombreColumna2}</h2>
-                            </div>
-                          ))}
-                        </Card.Body>
-                      </Card>
-                    </Col>
-                  </Row>
-                )}
-              </Card.Body>
-            </Card>
-          </div>
+                  </Card.Header>
+                  <Card.Body>
+                    {/* Tarjetas de accesos directos */}
+                    <Row xs={1} md={2} lg={4} className='g-4'>
+                      {[
+                        {
+                          nombre: 'Reporte Total',
+                          ruta: '/reporteTotal',
+                          icono: FaArrowRight,
+                        },
+                        {
+                          nombre: 'Estado Resultados',
+                          ruta: '/estadoResultados',
+                          icono: FaArrowRight,
+                        },
+                        {
+                          nombre: 'Ramo Administrativas',
+                          ruta: '/ramoAdministrativo',
+                          icono: FaArrowRight,
+                        },
+                        {
+                          nombre: 'Ramo Caución',
+                          ruta: '/ramoCaucion',
+                          icono: FaArrowRight,
+                        },
+                        {
+                          nombre: 'Ramo Crédito',
+                          ruta: '/ramoCredito',
+                          icono: FaArrowRight,
+                        },
+                        {
+                          nombre: 'Ramo Judicial',
+                          ruta: '/ramoJudicial',
+                          icono: FaArrowRight,
+                        },
+                        {
+                          nombre: 'Ramo Fidelidad',
+                          ruta: '/ramoFidelidad',
+                          icono: FaArrowRight,
+                        },
+                      ].map((acceso, idx) => (
+                        <Col key={idx}>
+                          <Card className='h-100 text-center hover-effect'>
+                            <Card.Body>
+                              <Card.Title>{acceso.nombre}</Card.Title>
+                              <Button
+                                as={Link}
+                                to={acceso.ruta}
+                                className='mt-3 button-custom-gradient'
+                              >
+                                Ir <acceso.icono />
+                              </Button>
+                            </Card.Body>
+                          </Card>
+                        </Col>
+                      ))}
+                    </Row>
+                  </Card.Body>
+                </Card>
+              </Col>
+            </Row>
+          </Container>
         ) : form.role === 'operador' ? (
-          //pantalla para operador
+          // Pantalla para operador
           <div>
             {' '}
             <Card
@@ -103,12 +121,11 @@ function Home({ form }) {
                 <FaUserCircle className='mb-2' size={35} /> Bienvenido operador{' '}
                 <strong>{form.username}</strong>
               </Card.Header>
-              <Card.Body>
-              </Card.Body>
+              <Card.Body></Card.Body>
             </Card>
           </div>
         ) : (
-          //pantalla para rol de consulta
+          // Pantalla para rol de consulta
           <>
             <Card
               className='mb-3'
@@ -119,17 +136,19 @@ function Home({ form }) {
             >
               <Card.Header className='d-flex align-items-center'>
                 <Col xs={12} sm={12} md={12} lg={12}>
-                  <FaUserCircle className='mb-2' size={35} />Bienvenido al perfil de consulta {' '}
+                  <FaUserCircle className='mb-2' size={35} />
+                  Bienvenido al perfil de consulta{' '}
                   <strong>{form.username}</strong>
                 </Col>
               </Card.Header>
-              <Card.Body>  <div className='d-flex justify-content-center'>
-            </div></Card.Body>
+              <Card.Body>
+                {' '}
+                <div className='d-flex justify-content-center'></div>
+              </Card.Body>
             </Card>
           </>
         )}
       </Container>
-     
     </>
   );
 }
